f8975f8ee9a911faf8d07533819f7b7b82399193
somil00 <95484425+somil00@users.noreply.github.com>
2023-08-15 19:33:36 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# RISC-V-Processor
+Implementation of RISC-V processor using Verilog in Xilinx Vivado
---
+++ RISC_processor/RISC_processor.srcs/sim_1/new/testbench.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/08/2022 11:10:53 PM
+// Design Name: 
+// Module Name: testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench(
+    input test_mips32
+    );
+endmodule
